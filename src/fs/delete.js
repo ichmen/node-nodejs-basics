fc214900bd8f3f760df4f03e67dbffd8ac3afd72
ichmen <ichmen@rambler.ru>
2023-06-13 16:51:59 +0300

    some task done

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,14 @@
+import fs from "fs";
+import path from "path";
+const fileName = "fileToRemove.txt";
+const directory = "files";
 const remove = async () => {
-    // Write your code here 
+  // Write your code here
+  fs.unlink(path.join(directory, fileName), (err) => {
+    if (err) {
+      throw new Error("FS operation failed");
+    }
+  });
 };
 
 await remove();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,22 @@
+import fs from "fs";
+import path from "path";
+const fileName = "wrongFilename.txt";
+const newFileName = "properFilename.md";
+const directory = "files";
 const rename = async () => {
-    // Write your code here 
+  // Write your code here
+  if (fs.existsSync(path.join(directory, newFileName))) {
+    throw new Error("FS operation failed");
+  }
+  fs.rename(
+    path.join(directory, fileName),
+    path.join(directory, newFileName),
+    (err) => {
+      if (err) {
+        throw new Error("FS operation failed");
+      }
+    }
+  );
 };
 
 await rename();
